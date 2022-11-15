ea085c86848e6dff5e8578f509bb9fad444291bb
xanyl <aniltiwari645@gmail.com>
2022-11-15 21:47:53 +0545

    Adding Bottom Navigation

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -10,20 +10,21 @@ import android.widget.Button;
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         btnLogin = findViewById(R.id.btnLogin);
-        setContentView(R.layout.activity_login);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(
-//                        LoginActivity.this, DashboardActivity.class
-//                );
-//                startActivity(intent);
+                Intent i = new Intent(
+                        LoginActivity.this,
+                        DashboardActivity.class
+                );
+                startActivity(i);
             }
         });
     }
