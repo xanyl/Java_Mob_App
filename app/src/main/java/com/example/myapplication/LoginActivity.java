d2ee37f4eae92762c637824225a36c642967ed7b
xanyl <aniltiwari645@gmail.com>
2022-11-15 21:24:09 +0545

    First Launch

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        setContentView(R.layout.activity_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(
+//                        LoginActivity.this, DashboardActivity.class
+//                );
+//                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -0,0 +1,70 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private  String username, fullname, email, password;
+    private EditText etUsername, etFullname, etEmail, etPassword;
+    private Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        //Variable name = findViewById(ID of View.xml file)
+        etUsername = findViewById(R.id.etUsername);
+        etFullname = findViewById(R.id.etFullname);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (meroValidation()) {
+                    Intent intent  = new Intent(SignupActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(SignupActivity.this, "Please enter the correct details", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private boolean meroValidation() {
+        username = etUsername.getText().toString();
+        fullname = etFullname.getText().toString();
+        email = etFullname.getText().toString();
+        password = etPassword.getText().toString();
+        if (username.isEmpty()) {
+            etUsername.setError("Username cannot be empty");
+            return false;
+        }
+        if (fullname.isEmpty()) {
+            etFullname.setError("Fullname cannot be empty");
+            return false;
+        }
+        if (email.isEmpty()) {
+            etEmail.setError("Email cannot be empty");
+            return false;
+        }
+        if (password.isEmpty()) {
+            etPassword.setError("Password cannot be empty");
+            return false;
+        }
+        return true;
+    }
+}
