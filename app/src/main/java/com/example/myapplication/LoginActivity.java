1bf4b4bbed452a03491f1d1a8291d78bae0388fa
xanyl <aniltiwari645@gmail.com>
2022-11-16 13:34:31 +0545

    Arranged Login Interface(Scrolling)

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -6,26 +6,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin;
+    private  String username, password;
+    private EditText etUsername, etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        etUsername = findViewById(R.id.etUsername);
+
+        etPassword = findViewById(R.id.etPassword);
+
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(
-                        LoginActivity.this,
-                        DashboardActivity.class
-                );
-                startActivity(i);
+               if(meroValidation()){
+                   Intent i = new Intent(
+                           LoginActivity.this,
+                           DashboardActivity.class
+                   );
+                   startActivity(i);
+               }
+               else{
+                   Toast.makeText(LoginActivity.this, "Please enter the correct details", Toast.LENGTH_SHORT).show();
+               }
             }
         });
+    }
+
+    private boolean meroValidation() {
+        username = etUsername.getText().toString();
+
+        password = etPassword.getText().toString();
+        if (username.isEmpty()) {
+            etUsername.setError("Username cannot be empty");
+            return false;
+        }
+
+        if (password.isEmpty()) {
+            etPassword.setError("Password cannot be empty");
+            return false;
+        }
+        return true;
     }
 }
