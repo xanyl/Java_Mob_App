51dc9f4d8e0575ace01a7d69f895af3d76cfd331
xanyl <aniltiwari645@gmail.com>
2022-11-30 13:49:43 +0545

    Api

---
+++ app/src/main/java/com/example/myapplication/response/FoodResponse.java
@@ -0,0 +1,35 @@
+package com.example.myapplication.response;
+
+import com.example.myapplication.models.Food;
+
+import java.util.ArrayList;
+
+public class FoodResponse {
+    private boolean success;
+    private String message;
+    private ArrayList<Food> data;    //json ma j xa api ko tyai rakhne ani array list ma array list
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ArrayList<Food> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<Food> data) {
+        this.data = data;
+    }
+}
