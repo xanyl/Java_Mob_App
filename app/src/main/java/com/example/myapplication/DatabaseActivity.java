676c4ea1bd48f1f6668287cdd8bc27df0de0e79f
xanyl <aniltiwari645@gmail.com>
2022-12-05 13:38:27 +0545

    item view created

--- app/src/main/java/com/example/myapplication/DatabaseActivity.java
+++ app/src/main/java/com/example/myapplication/DatabaseActivity.java
@@ -1,35 +1,43 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-import com.example.myapplication.app.App;
-import com.example.myapplication.database.AppDatabase;
-import com.example.myapplication.database.dao.FoodDao;
-import com.example.myapplication.database.model.FoodEntity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.myapplication.databinding.ActivityDatabaseBinding;
 
 public class DatabaseActivity extends AppCompatActivity {
-    private AppDatabase db;
-    private FoodDao foodDao;
+    private ActivityDatabaseBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_database);
-        db = App.db;
-        foodDao = db.foodDao();
+        binding = ActivityDatabaseBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        FoodEntity food1 = new FoodEntity();
-        food1.setName("Burger");
-        food1.setFood_id(23);
-        food1.setPrice("250");
-        food1.setStatus("Available");
 
-        //inserting data into database
-//        foodDao.insertItem(food1);
-        foodDao.deleteItem(food1);
-        Log.d("TEST_FOOD","DB Record Size ==>"+ foodDao.getAllItems().size());
+        binding.btnAddRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DatabaseActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.rvRecords.setLayoutManager(new LinearLayoutManager(
+                this,LinearLayoutManager.VERTICAL,
+                false
+        ));
+
+        binding.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DatabaseActivity.this, "Delete Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
